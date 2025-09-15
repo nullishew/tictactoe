f45af545496a9d65b6c7a2543eae31b676778dff
nullishew <155263075+nullishew@users.noreply.github.com>
2025-09-15 14:05:18 -0400

    Refactor + Created method to place pieces of tictactoe board
    Renamed playerBoards variable in TicTacToe.java to playerPositions to maintain consistency.
    
    Created and successfully tested method that attempts to place pieces for a given player on a given cell on the tictactoe board, returning true if the operation is successful and false if a piece is already in that cell or the player is not valid or the cell is not on the board

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -1,18 +1,18 @@
 package tictactoe;
 
 public class TicTacToe {
-  private int[] playerBoards;
+  private int[] playerPositions;
   private int totalTurns;
   private int playerTurn;
 
   public TicTacToe() {
-    playerBoards = new int[] { 0, 0 };
+    playerPositions = new int[] { 0b000000000, 0b000000000 };
     totalTurns = 0;
     playerTurn = 0;
   }
 
   public int getPlayerPosition(int playerId) {
-    return playerBoards[playerId];
+    return playerPositions[playerId];
   }
 
   public int getTotalTurns() {
@@ -21,6 +21,29 @@ public class TicTacToe {
 
   public int getPlayerTurn() {
     return playerTurn;
+  }
+
+  public boolean isInBounds(int cellId) {
+    return 0 <= cellId && cellId <= 8;
+  }
+
+  public boolean isValidPlayer(int playerId) {
+    return playerId == 0 || playerId == 1;
+  }
+
+  public boolean trySetCell(int cellId, int playerId) {
+    if (!isInBounds(cellId) || !isValidPlayer(playerId)) {
+      return false;
+    }
+    int mask = 0b1 << cellId;
+    if ((playerPositions[0] & mask) != 0) {
+      return false;
+    }
+    if ((playerPositions[1] & mask) != 0) {
+      return false;
+    }
+    playerPositions[playerId] ^= mask;
+    return true;
   }
 
 }
--- test/tictactoe/TicTacToeTest.java
+++ test/tictactoe/TicTacToeTest.java
@@ -11,4 +11,70 @@ public class TicTacToeTest {
     assertEquals(ticTacToe.getPlayerPosition(0), 0);
     assertEquals(ticTacToe.getPlayerPosition(1), 0);
   }
+
+  @Test
+  public void testTrySetCell() {
+    // test placing pieces out of bounds
+    TicTacToe ticTacToe = new TicTacToe();
+    assertEquals(ticTacToe.trySetCell(-1, 0), false);
+    assertEquals(ticTacToe.trySetCell(-1, 1), false);
+    assertEquals(ticTacToe.trySetCell(9, 0), false);
+    assertEquals(ticTacToe.trySetCell(9, 1), false);
+
+    // test placing pieces for a player that does not exist
+    ticTacToe = new TicTacToe();
+    assertEquals(ticTacToe.trySetCell(0, 0), true);
+    ticTacToe = new TicTacToe();
+    assertEquals(ticTacToe.trySetCell(0, -1), false);
+    ticTacToe = new TicTacToe();
+    assertEquals(ticTacToe.trySetCell(0, 2), false);
+    ticTacToe = new TicTacToe();
+    assertEquals(ticTacToe.trySetCell(0, 67), false);
+
+    // test setting empty cells to player 0
+    ticTacToe = new TicTacToe();
+    for (int cellId = 0; cellId < 9; cellId++) {
+      assertEquals(ticTacToe.trySetCell(cellId, 0), true);
+    }
+    // test setting empty cells to player 0 then attempting to set cells player
+    // player 0 and then player 1
+    ticTacToe = new TicTacToe();
+    for (int cellId = 0; cellId < 9; cellId++) {
+      assertEquals(ticTacToe.trySetCell(cellId, 0), true);
+      assertEquals(ticTacToe.trySetCell(cellId, 0), false);
+      assertEquals(ticTacToe.trySetCell(cellId, 1), false);
+    }
+    // test setting empty cells to player 0 then attempting to set cells player
+    // player 1 and then player 0
+    ticTacToe = new TicTacToe();
+    for (int cellId = 0; cellId < 9; cellId++) {
+      assertEquals(ticTacToe.trySetCell(cellId, 0), true);
+      assertEquals(ticTacToe.trySetCell(cellId, 1), false);
+      assertEquals(ticTacToe.trySetCell(cellId, 0), false);
+    }
+
+    // test setting empty cells to player 1
+    TicTacToe ticTacToe2 = new TicTacToe();
+    for (int cellId = 0; cellId < 9; cellId++) {
+      assertEquals(ticTacToe2.trySetCell(cellId, 1), true);
+    }
+
+    // test setting empty cells to player 1 then attempting to set cells player
+    // player 0 and then player 1
+    ticTacToe2 = new TicTacToe();
+    for (int cellId = 0; cellId < 9; cellId++) {
+      assertEquals(ticTacToe2.trySetCell(cellId, 1), true);
+      assertEquals(ticTacToe2.trySetCell(cellId, 0), false);
+      assertEquals(ticTacToe2.trySetCell(cellId, 1), false);
+    }
+
+    // test setting empty cells to player 1 then attempting to set cells player
+    // player 1 and then player 0
+    ticTacToe2 = new TicTacToe();
+    for (int cellId = 0; cellId < 9; cellId++) {
+      assertEquals(ticTacToe2.trySetCell(cellId, 1), true);
+      assertEquals(ticTacToe2.trySetCell(cellId, 1), false);
+      assertEquals(ticTacToe2.trySetCell(cellId, 0), false);
+    }
+  }
 }
