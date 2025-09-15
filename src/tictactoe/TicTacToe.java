bcb2f8b7437803e7c52a77d1238f0d082bae1700
nullishew <155263075+nullishew@users.noreply.github.com>
2025-09-15 13:42:24 -0400

    Verified TicTacToe board initialization
    Created a constructor for the TicTacToe class to initialize a bitmap for each players' pieces and keep track of which player's turn it is and how many turns have elapsed.
    
    Successfully created and passed unit tests to check initialization.

--- src/tictactoe/TicTacToe.java
+++ src/tictactoe/TicTacToe.java
@@ -1,5 +1,26 @@
 package tictactoe;
 
 public class TicTacToe {
-  
+  private int[] playerBoards;
+  private int totalTurns;
+  private int playerTurn;
+
+  public TicTacToe() {
+    playerBoards = new int[] { 0, 0 };
+    totalTurns = 0;
+    playerTurn = 0;
+  }
+
+  public int getPlayerPosition(int playerId) {
+    return playerBoards[playerId];
+  }
+
+  public int getTotalTurns() {
+    return totalTurns;
+  }
+
+  public int getPlayerTurn() {
+    return playerTurn;
+  }
+
 }
--- test/tictactoe/TicTacToeTest.java
+++ test/tictactoe/TicTacToeTest.java
@@ -1,5 +1,14 @@
 package tictactoe;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class TicTacToeTest {
-  
+  @Test
+  public void testTicTacToeInitialization() {
+    TicTacToe ticTacToe = new TicTacToe();
+    assertEquals(ticTacToe.getPlayerPosition(0), 0);
+    assertEquals(ticTacToe.getPlayerPosition(1), 0);
+  }
 }
